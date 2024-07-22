fe4413f465d92118e419bf119fe30947d90e6b39
mazxcv <mazxccc@gmail.com>
2024-07-22 21:22:41 +0300

    feature-init init logler

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"cthulhu/internal/config"
-	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	// init config/ cleanenv (умеет читать с разных расширений. struct tag)
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
-
-	// TODO: init logger/ slog
+	log := setupLogger(cfg.Env)
+	log.Info("starting cthulhu", slog.String("env", cfg.Env))
+	log.Debug("debugging messages are enabled")
 
 	// TODO: init storage/ sqlite
 
 	// TODO: init router/ chi, "chi render"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+
 }
