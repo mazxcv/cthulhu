b49ee9065d363e9f3d9b73bf70acd03812d4d2f7
mazxcv <mazxccc@gmail.com>
2024-07-23 00:39:46 +0300

    #m.ugrin #feature-busines add pretty logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"cthulhu/internal/config"
 	mwLogger "cthulhu/internal/http-server/middleware/logger"
+	"cthulhu/internal/lib/logger/handlers/slogpretty"
 	"cthulhu/internal/lib/logger/sl"
 	"cthulhu/internal/storage/sqlite"
 	"log/slog"
@@ -22,13 +23,18 @@ func main() {
 	// init config/ cleanenv (умеет читать с разных расширений. struct tag)
 	cfg := config.MustLoad()
 	log := setupLogger(cfg.Env)
-	log.Info("starting cthulhu", slog.String("env", cfg.Env))
+	log.Info("Information starting cthulhu", slog.String("env", cfg.Env))
+	log.Debug("Debug starting cthulhu", slog.String("env", cfg.Env))
+	log.Warn("Warning starting cthulhu", slog.String("env", cfg.Env))
+	log.Error("Error starting cthulhu", slog.String("env", cfg.Env))
 
 	storageCTX, err := sqlite.New(cfg.StoragePath)
 	if err != nil {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
+
+	_ = storageCTX
 
 	router := chi.NewRouter()
 	router.Use(middleware.RequestID)
@@ -44,13 +50,24 @@ func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 	switch env {
 	case envLocal:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = setupPrettySlog()
 	case envDev:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = setupPrettySlog()
 	case envProd:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	}
 
 	return log
 
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+	return slog.New(handler)
 }
--- go.mod
+++ go.mod
@@ -9,7 +9,14 @@ require (
 )
 
 require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
+
+require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/fatih/color v1.17.0
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
